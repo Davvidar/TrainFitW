e222789ef73e10d47e5c567f2b284fe67e124529
Davvidar <daargente@gmail.com>
2024-08-27 13:07:39 +0200

    fix

--- src/components/ImageZoom/ImageZoom.jsx
+++ src/components/ImageZoom/ImageZoom.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import fondo from '../../assets/images/fondo.png';
 import fondo2 from '../../assets/images/fondo2.png';
 import './ImageZoom.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 const ImageZoom = () => {
   const navigate = useNavigate();
@@ -23,7 +23,7 @@ const ImageZoom = () => {
           <h1 className="title">Calculadora Kcal</h1>
           <div className="description">
             <p>Con esta calculadora podrás hacer una estimación de tu ingesta de kcal diarias.</p>
-            <button onClick={handleButtonKcal}>Ir a la calculadora </button>
+            <button><NavLink to="/calculadoras/calculadoraKcal">Ir a la calculadora</NavLink> </button>
           </div>
         </div>
       </div>
@@ -33,7 +33,7 @@ const ImageZoom = () => {
           <h1 className="title">Calculadora RM</h1>
           <div className="description">
             <p>Con esta calculadora podrás hacer una estimación de tu RM (repetición maxima) en cualquier ejercicio.</p>
-            <button onClick={handleButtonRm}>Ir a la calculadora </button>
+            <button><NavLink to="/calculadoras/calculadoraKcal">Ir a la calculadora</NavLink> </button>
           </div>
         </div>
       </div>
