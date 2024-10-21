925f575abc882c3fb525762bcbf5f3f431da5526
Davvidar <daargente@gmail.com>
2024-10-22 00:08:48 +0200

    all links added

--- src/Pages/MorePage.jsx
+++ src/Pages/MorePage.jsx
@@ -9,20 +9,20 @@ const MorePage = () => {
 
       {/* Links para descargar la app */}
       <div className="download-buttons">
-        <a 
-          href="https://play.google.com/store/apps/details?id=com.trainfit.trainfit&fbclid=PAZXh0bgNhZW0CMTEAAabKa5R3sc2ZUqxy39Tgqw7qOQdB96SJHETbJHpCCo4ClfmWy_-lkuwRc_o_aem_zkL4C3LBk-lq6Q_S4KWVQw" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://play.google.com/store/apps/details?id=com.trainfit.trainfit&fbclid=PAZXh0bgNhZW0CMTEAAabKa5R3sc2ZUqxy39Tgqw7qOQdB96SJHETbJHpCCo4ClfmWy_-lkuwRc_o_aem_zkL4C3LBk-lq6Q_S4KWVQw"
+          target="_blank"
+          rel="noopener noreferrer"
           className="action-button google-play"
         >
           <img src={google} alt="Google Play" className="store-logo" />
           <span className="link-text">Descarga en Google Play</span>
         </a>
 
-        <a 
-          href="https://testflight.apple.com/join/6WnM77NZ" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://testflight.apple.com/join/6WnM77NZ"
+          target="_blank"
+          rel="noopener noreferrer"
           className="action-button app-store"
         >
           <img src={apple} alt="App Store" className="store-logo" />
@@ -34,10 +34,10 @@ const MorePage = () => {
 
       {/* Links para redes sociales */}
       <div className="download-buttons">
-        <a 
-          href="https://www.instagram.com/trainfit.app/" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.instagram.com/trainfit.app/"
+          target="_blank"
+          rel="noopener noreferrer"
           className="action-button instagram"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-instagram store-logo" viewBox="0 0 16 16">
@@ -46,10 +46,10 @@ const MorePage = () => {
           <span className="link-text">Instagram</span>
         </a>
 
-        <a 
-          href="https://www.tiktok.com/@trainfit.app?lang=es" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.tiktok.com/@trainfit.app?lang=es"
+          target="_blank"
+          rel="noopener noreferrer"
           className="action-button tiktok"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-tiktok store-logo" viewBox="0 0 16 16">
@@ -58,10 +58,10 @@ const MorePage = () => {
           <span className="link-text">TikTok</span>
         </a>
 
-        <a 
-          href="https://www.facebook.com/AppTrainFit" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.facebook.com/AppTrainFit"
+          target="_blank"
+          rel="noopener noreferrer"
           className="action-button facebook"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" className="bi bi-facebook store-logo" viewBox="0 0 16 16">
@@ -70,19 +70,19 @@ const MorePage = () => {
           <span className="link-text">Facebook</span>
         </a>
 
-        <a 
-          href="https://www.youtube.com/@trainfit" 
-          target="_blank" 
-          rel="noopener noreferrer" 
+        <a
+          href="https://www.youtube.com/@trainfit"
+          target="_blank"
+          rel="noopener noreferrer"
           className="action-button youtube"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-youtube store-logo" viewBox="0 0 16 16">
-  <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
-</svg>
+            <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z" />
+          </svg>
           <span className="link-text">YouTube</span>
         </a>
       </div>
-      
+
     </div>
   );
 };
